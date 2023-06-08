a300f56861c18ddbcf270266dbe3475845ebb3e1
tungtung <tungvtse160146@fpt.edu.vn>
2023-06-08 09:40:13 +0700

    m

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -1,7 +1,6 @@
 import React, { createContext, useMemo, useContext } from "react";
 import { io } from "socket.io-client";
 
-
 const SocketContext = createContext(null);
 
 export const useSocket = () => {
@@ -10,8 +9,16 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
- 
-  const socket = useMemo(() => io(`${process.env.REACT_APP_URL}`), []);
+  const socket = useMemo(
+    () =>
+      io(`${process.env.REACT_APP_URL}`, {
+        auth: {
+          accessToken:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDc5NmNiZjliMDYxNjk2Y2YzNDE2NmEiLCJwbGFuIjoiZGVmYXVsdCIsImlhdCI6MTY4NTcwMDgwNSwiZXhwIjoxNjg1NzA4MDA1fQ.xwu0V46M0Md4mGPg9WsOotIaXpVfDgs6mjg3EMXpa7g",
+        },
+      }),
+    []
+  );
 
   return (
     <SocketContext.Provider value={socket}>
